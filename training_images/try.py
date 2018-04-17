d696979cff3aca8abf79d6d54d63902c4d70de0f
Juan Moreno <=>
2018-04-16 20:50:13 -0500

    se terminaron todos los casos con el algoritmo de python

--- training_images/try.py
+++ training_images/try.py
@@ -4,5 +4,5 @@ import argparse
 import cv2
 import os
 
-for i in range(1,10000):
+for i in range(4746,10001):
     print(i,pytesseract.image_to_string(Image.open(str(i)+".jpg") ,config='-c tessedit_char_whitelist=abcdef0123456789') )
