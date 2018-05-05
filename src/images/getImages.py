3c4a119cb4df999011760caaa8f8271027de4897
Juan Moreno <juan.andres.208@hotmail.com>
2018-05-05 18:51:25 -0500

    scrip para sacar imagenes y subirlas a sharelatex

---
+++ src/images/getImages.py
@@ -0,0 +1,6 @@
+x= [1,9,4,11,17,23,29,42,46,47,51,58,63,69,71,74,77,78,81,87,83,91,92,95,102,103]
+path= "/home/blackphantom/proyectos/Captcha-With-Neuronal-Network/src/images/"
+directory = "out/2/"
+for i in x:
+    print("cp "+directory+str(i)+".jpg "+"toUpload/")
+
